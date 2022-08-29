a9061545a8a642b794bbe605fc887af8182d9eed
Kemil Belcaries <belcaries@gmail.com>
2022-08-29 10:59:52 -0600

    this is a test

--- myApp.js
+++ myApp.js
@@ -5,19 +5,21 @@ require('dotenv').config()
 app.use(express.static(__dirname+ '/public'))
 app.use('/public',express.static(__dirname+ '/public'))
 
-let response = "Hello World".toUpperCase()
-if(process.env.MESSAGE_STYLE ==='allCaps'){
-  response = "Hello World".toUpperCase()
-}else{
-  response = "Hello World"
-}
+
+
 
 app
   .get('/',(req,res)=>{
   res.sendFile(__dirname + "/views/index.html")
 })
   .get('/json',(req,res)=>{
-  res.json({"message": response})
+
+  if(process.env.MESSAGE_STYLE ==='uppercase'){
+      response = "Hello World".toUpperCase()
+    }else{
+      response = "Hello World"
+    }
+  res.json({"message": process.env.MESSAGE_STYLE})
 })
 console.log('Hello World');
 
