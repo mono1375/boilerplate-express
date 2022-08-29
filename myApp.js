bc3c85745bed5965f635bed1425102b5d06116e6
Kemil Belcaries <belcaries@gmail.com>
2022-08-29 10:02:01 -0600

    add public assets to static

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ let express = require('express');
 let app = express();
 
 app.use(express.static(__dirname+ '/public'))
+app.use('/public',express.static(__dirname+ '/public'))
 
 
 app.get('/',(req,res)=>{
