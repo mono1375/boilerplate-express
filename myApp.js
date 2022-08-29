a0db03f140085ce1b50b0206d45323082a93e485
Kemil Belcaries <belcaries@gmail.com>
2022-08-29 10:20:56 -0600

    add .env file

--- myApp.js
+++ myApp.js
@@ -1,9 +1,16 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
 app.use(express.static(__dirname+ '/public'))
 app.use('/public',express.static(__dirname+ '/public'))
 
+let response = "Hello World".toUpperCase()
+if(process.env.MESSAGE_STYLE ==='allCaps'){
+  response = "Hello World".toUpperCase()
+}else{
+  response = "Hello World"
+}
 
 app
   .get('/',(req,res)=>{
