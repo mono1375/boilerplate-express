9ec8631b0aad9af6f4e93012de22daca96d43143
Kemil Belcaries <belcaries@gmail.com>
2022-08-29 19:47:16 -0600

    trying new metod for middelware

--- myApp.js
+++ myApp.js
@@ -5,13 +5,11 @@ let app = express();
 app.use(express.static(__dirname+ '/public'))
 app.use('/public',express.static(__dirname+ '/public'))
 
-app.use(function middleware(req,res,next){
-  var string = req.method + ' ' + req.path +'-'+req.ip
-  console.log(string);
+
+app.use((req,res,next)=>{
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 })
-
-
 app
   .get('/',(req,res)=>{
   res.sendFile(__dirname + "/views/index.html")
