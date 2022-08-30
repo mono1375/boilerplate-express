a0b5dd69c5cddeb1e82425b4365b817fc1dac9aa
Kemil Belcaries <belcaries@gmail.com>
2022-08-29 20:23:08 -0600

    change name request

--- myApp.js
+++ myApp.js
@@ -32,14 +32,16 @@ app
       echo:word
     })
   })
-  .get('/name',()=>{
+app.get("/name", function(req, res) {
     var firstName = req.query.first;
-    var lastName = req.query.last; 
+    var lastName = req.query.last;
+    // OR you can destructure and rename the keys
+    var { first: firstName, last: lastName } = req.query;
+    // Use template literals to form a formatted string
     res.json({
       name: `${firstName} ${lastName}`
-    })
-  })
-
+    });
+  });
 const middleware = (req,res,next)=>{
   req.time =  new Date().toString()
   next()
